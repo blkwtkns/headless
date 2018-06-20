4ca16c89a1e9912b0f974f0fc18d96ac74ff6d32
Blake Watkins <iamsoblake@gmail.com>
2018-06-20 16:37:35 -0700

    Setting parameters on performing action

--- execute_votes.sh
+++ execute_votes.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-for x in {1..30} ; do node index.js ||break;sleep 2; done
+# for x in {1..30} ; do node index.js ||break;sleep 2; done
+for x in {1..30} ; do node index.js ||break; done
--- index.js
+++ index.js
@@ -6,21 +6,64 @@ const agents = require('browser-agents');
 // or 0 to turn off the limit
 // emitter.setMaxListeners(0)
 
-const selector = 'li#my_db_entry_191140347>div.item_vote>a';
+// process.on('uncaughtException', function (err) {
+//   console.error(err);
+//   console.log("Node NOT Exiting...");
+// });
+
+const bronx = 'li#my_db_entry_191140347>div.item_vote>a';
+const votesList = '.votes';
 const dest = 'https://a.pgtb.me/RbrpXd/n1kPk?w=67644150&e=191140347';
 
-const vote = async () => {
-  console.log('voting');
+const vote = async (voteNumber = 0) => {
+  console.log('voting', voteNumber);
   const options = { executablePath: '/usr/bin/google-chrome-unstable' };
   const browser = await puppeteer.launch(options);
   // const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   const userAgent = agents.random();
-  await page.setUserAgent(userAgent);
-  await page.goto(dest, {waitUntil: 'networkidle2'});
+
+  try {
+    await page.setUserAgent(userAgent);
+  } catch(err) {
+    console.log('userAgent not handled', err)
+  }
+
+  try {
+    await page.goto(dest, {waitUntil: 'networkidle2'});
+  } catch(err) {
+    console.log('goto ops not handled', err)
+  }
   // await page.focus(selector);
-  await page.waitFor(2000);
-  await page.click(selector);
+  try {
+    await page.waitFor(2000);
+  } catch(err) {
+    console.log('wait ops not handled', err)
+  }
+
+  try {
+    const votes = await page.$$(votesList);
+    // const bronxVote = await page.$$('li#my_db_entry_191140347>div.item_vote');
+    const voteTally = votes.length;
+    let voteList = [];
+    let data;
+    for (let i = 0; i < voteTally; i++) {
+      data = await (await votes[i].getProperty('innerHTML')).jsonValue();
+      voteList.push(data);
+    }
+
+    console.log('check tally', voteList.sort((a,b) => b - a))
+  } catch(err) {
+    console.log('selection not handled', err)
+  }
+
+  // click element
+  // try {
+  //   await page.click(bronx);
+  // } catch(err) {
+  //   console.log('selection not handled', err)
+  // }
+
   // await page.screenshot({
   //   path: 'proof.png',
   //   fullPage: true
@@ -28,6 +71,7 @@ const vote = async () => {
   browser.close();
 };
 
-for (let i = 0; i < 10; i++) {
-  setTimeout(vote, 1000);
-}
+vote();
+// for (let i = 0; i < 10; i++) {
+//   setTimeout(() => vote(i), 1);
+// }
